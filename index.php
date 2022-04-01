9a4f983aa666172bf09c9b7e22a82a2f9026ad3a
Yiwen <yangyiwen1999@outlook.com>
2022-04-01 11:13:40 +1100

    Added str_ends_with implementation for older php

--- index.php
+++ index.php
@@ -14,6 +14,13 @@
             echo '<option value="'.$key.'">'.$value->display_name.'</option>';
         }
     }
+    if (! function_exists('str_ends_with')) {
+    function str_ends_with(string $haystack, string $needle): bool
+        {
+            $needle_len = strlen($needle);
+            return ($needle_len === 0 || 0 === substr_compare($haystack, $needle, - $needle_len));
+        }
+    }
 ?>
 <html>
     <!-- HEAD -->
